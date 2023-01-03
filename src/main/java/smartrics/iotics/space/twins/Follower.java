aa0f8a5e12334ee72354d998d678720364078937
smartrics <fabrizio.cannizzo@iotic-labs.com>
2023-01-03 22:47:51 +0000

    added blocking method for follow

--- src/main/java/smartrics/iotics/space/twins/Follower.java
+++ src/main/java/smartrics/iotics/space/twins/Follower.java
@@ -3,13 +3,29 @@ package smartrics.iotics.space.twins;
 import com.google.protobuf.BoolValue;
 import com.iotics.api.*;
 import io.grpc.stub.StreamObserver;
+import org.jetbrains.annotations.NotNull;
 import smartrics.iotics.space.Builders;
+
+import java.util.Iterator;
 
 public interface Follower extends Identifiable {
 
     InterestAPIGrpc.InterestAPIStub getInterestAPIStub();
 
+    InterestAPIGrpc.InterestAPIBlockingStub getInterestAPIBlockingStub();
+
     default void follow(FeedID feedId, StreamObserver<FetchInterestResponse> responseStreamObserver)  {
+        FetchInterestRequest request = newRequest(feedId);
+        getInterestAPIStub().fetchInterests(request, responseStreamObserver);
+    }
+
+    default Iterator<FetchInterestResponse> follow(FeedID feedId)  {
+        FetchInterestRequest request = newRequest(feedId);
+        return getInterestAPIBlockingStub().fetchInterests(request);
+    }
+
+    @NotNull
+    private FetchInterestRequest newRequest(FeedID feedId) {
         FetchInterestRequest request = FetchInterestRequest.newBuilder()
                 .setHeaders(Builders.newHeadersBuilder(getAgentIdentity().did()).build())
                 .setFetchLastStored(BoolValue.newBuilder().setValue(true).build())
@@ -20,7 +36,7 @@ public interface Follower extends Identifiable {
                                 .build())
                         .build())
                 .build();
-        getInterestAPIStub().fetchInterests(request, responseStreamObserver);
+        return request;
     }
 
 }
